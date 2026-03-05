65b0c55132ddd51a14dfbe9be7765e58e3b56e93
Iggy Gonzales <migueljust4school@gmail.com>
2026-03-05 13:36:12 -0500

    fix: bat file encoding, dbt pipeline fully automated

--- run_fetcher.bat
+++ run_fetcher.bat
@@ -1,4 +1,8 @@
 @echo off
-cd "C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline"
-call venv\Scripts\activate
-python -m ingestion.noaa_fetcher
+chcp 65001 >nul
+set PYTHONIOENCODING=utf-8
+cd /d "C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline"
+call "C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline\venv\Scripts\activate.bat"
+"C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline\venv\Scripts\python.exe" -m ingestion.noaa_fetcher >> "C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline\logs\fetcher.log" 2>&1
+cd "C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline\transforms\snow_transforms"
+"C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline\transforms\venv\Scripts\dbt.exe" run >> "C:\Users\migue\OneDrive\Documents\NEU Projects\snowboard_pipeline\logs\fetcher.log" 2>&1
